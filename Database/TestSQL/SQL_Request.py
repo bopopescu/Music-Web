c69ed7eb4cb8f2f93e0dcd8aea8626046e072350
Andrew Rose <Andyrose1232@gmail.com>
2020-05-18 12:09:45 -0500

    Testing stuff

--- Database/TestSQL/SQL_Request.py
+++ Database/TestSQL/SQL_Request.py
@@ -2,13 +2,14 @@ import mysql.connector
 from mysql.connector import Error
 
 
-def create_connection(host_name, user_name, user_password):
+def create_connection(host_name, user_name, user_password, database):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
+            database=database
         )
         print("Connection to MySQL DB successful")
     except Error as e:
@@ -17,5 +18,18 @@ def create_connection(host_name, user_name, user_password):
     return connection
 
 
-connection = create_connection("music-web.mysql.database.azure.com", "DataAdmin@music-web", "Kriegel2021")
+connection = create_connection("music-web.mysql.database.azure.com", "DataAdmin@music-web", "Kriegel2021", "testdatabaseandy")
+
+mycursor = connection.cursor()
+
+mycursor.execute("CREATE DATABASE whyisthisnotworking")
+
+#mycursor.execute("CREATE TABLE testtable (name VARCHAR(255), address VARCHAR(255))")
+
+#mycursor.execute("SHOW TABLES")
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)
 
